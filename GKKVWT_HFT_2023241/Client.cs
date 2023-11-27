eff32aa8774787dae46faf8f6012e8bc9bf51eff
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-11-27 03:20:22 +0100

    Teszt

--- GKKVWT_HFT_2023241/Client.cs
+++ GKKVWT_HFT_2023241/Client.cs
@@ -7,6 +7,7 @@ namespace GKKVWT_HFT_2023241.Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Console.WriteLine("Using GIT!!!");
         }
     }
 }
