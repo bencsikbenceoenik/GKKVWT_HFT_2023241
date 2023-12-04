1741ee7041c09733d58be9c32837496603cac4d8
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-04 21:27:29 +0100

    SongDbContext Dbsets added.

--- GKKVWT_HFT_2023241.Repository/Database/SongDbContext.cs
+++ GKKVWT_HFT_2023241.Repository/Database/SongDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GKKVWT_HFT_2023241.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace GKKVWT_HFT_2023241.Repository.Database
 {
     public class SongDbContext : DbContext
     {
-
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Label> Labels { get; set; }
     }
 }
