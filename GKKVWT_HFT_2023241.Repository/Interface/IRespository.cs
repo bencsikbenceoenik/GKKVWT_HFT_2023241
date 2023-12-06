078739c8c054e2a89d9fcc6cbcef5982166642b9
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-06 17:58:15 +0100

    IRepository interface functions added.

--- GKKVWT_HFT_2023241.Repository/Interface/IRespository.cs
+++ GKKVWT_HFT_2023241.Repository/Interface/IRespository.cs
@@ -1,7 +1,13 @@
-﻿namespace GKKVWT_HFT_2023241.Repository.Interface
+﻿using System.Linq;
+
+namespace GKKVWT_HFT_2023241.Repository.Interface
 {
     public interface IRespository<T> where T : class
     {
-
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
     }
 }
