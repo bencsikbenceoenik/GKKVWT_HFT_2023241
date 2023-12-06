60de85b608ba5243f8d7ad19ba42b0cebf825e22
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-06 18:36:55 +0100

    ISongLogic interface CRUD functions added.

--- GKKVWT_HFT_2023241.Logic/Interfaces/ISongLogic.cs
+++ GKKVWT_HFT_2023241.Logic/Interfaces/ISongLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GKKVWT_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace GKKVWT_HFT_2023241.Logic.Interfaces
 {
     public interface ISongLogic
     {
+        void Create(Song item);
+        void Delete(int id);
+        Song Read(int id);
+        IQueryable<Song> ReadAll();
+        void Update(Song item);
     }
 }
