e2413f1f7c7d021d81e728fb6e0994604048bb61
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-06 19:05:02 +0100

    LabelLogic CRUD functions and Constructor implemented. Dependency Injection added.

--- GKKVWT_HFT_2023241.Logic/Classes/LabelLogic.cs
+++ GKKVWT_HFT_2023241.Logic/Classes/LabelLogic.cs
@@ -1,4 +1,6 @@
 ﻿using GKKVWT_HFT_2023241.Logic.Interfaces;
+using GKKVWT_HFT_2023241.Models;
+using GKKVWT_HFT_2023241.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,35 @@ namespace GKKVWT_HFT_2023241.Logic.Classes
 {
     public class LabelLogic : ILabelLogic
     {
+        IRepository<Label> repo;
+        public LabelLogic(IRepository<Label> repo)
+        {
+            this.repo = repo;
+        }
 
+        public void Create(Label item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Label Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Label> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Label item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
