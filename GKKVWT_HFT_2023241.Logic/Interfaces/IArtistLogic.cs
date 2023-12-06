e69ce00dc09051041699da5b3097d2d01d2d9339
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-06 18:36:03 +0100

    IArtistLogic interface CRUD functions added.

--- GKKVWT_HFT_2023241.Logic/Interfaces/IArtistLogic.cs
+++ GKKVWT_HFT_2023241.Logic/Interfaces/IArtistLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GKKVWT_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace GKKVWT_HFT_2023241.Logic.Interfaces
 {
     public interface IArtistLogic
     {
+        void Create(Artist item);
+        void Delete(int id);
+        Artist Read(int id);
+        IQueryable<Artist> ReadAll();
+        void Update(Artist item);
     }
 }
