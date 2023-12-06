3d9e5535297b5c62c7d5b0f3811b3687dff2b594
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-06 21:22:00 +0100

    Test method added for GetSongsByDurationAndArtistGender.

--- GKKVWT_HFT_2023241.Test/SongLogicTester.cs
+++ GKKVWT_HFT_2023241.Test/SongLogicTester.cs
@@ -14,21 +14,61 @@ namespace GKKVWT_HFT_2023241.Test
     public class SongLogicTester
     {
         SongLogic logic;
-        Mock<IRepository<Song>> mockMovieRepo;
+        Mock<IRepository<Song>> mockSongRepo;
 
         [SetUp]
         public void Init()
         {
-            mockMovieRepo = new Mock<IRepository<Song>>();
-            mockMovieRepo.Setup(m => m.ReadAll()).Returns(new List<Song>()
+            mockSongRepo = new Mock<IRepository<Song>>();
+            mockSongRepo.Setup(m => m.ReadAll()).Returns(new List<Song>()
             {
                 new Song("1#SongA#Type#2016-09-14#200#English#1#2#"),
                 new Song("2#SongB#Type#2016-12-30#193#English#2#1#"),
                 new Song("3#SongC#Type#2020-01-11#212#Korean#3#3#"),
                 new Song("4#SongD#Type#2022-05-01#224#Korean#4#4#"),
             }.AsQueryable());
-            logic = new SongLogic(mockMovieRepo.Object);
+            logic = new SongLogic(mockSongRepo.Object);
         }
+
+        
+
+        [Test]
+        public void GetSongsByDurationAndArtistGender_ShouldReturnFilteredSongs()
+        {
+            // Arrange
+            var mockArtistRepository = new Mock<IRepository<Artist>>();
+            var mockSongRepository = new Mock<IRepository<Song>>();
+
+            var service = new SongLogic(mockSongRepository.Object, mockArtistRepository.Object);
+
+            // Mock data
+            var artists = new List<Artist>
+            {
+                new Artist { ArtistId = 1, Gender = "Male" },
+                new Artist { ArtistId = 2, Gender = "Female" },
+                new Artist { ArtistId = 3, Gender = "Male" },
+            };
+
+            var songs = new List<Song>
+            {
+                new Song { ArtistId = 1, Duration = 120 },
+                new Song { ArtistId = 2, Duration = 90 },
+                new Song { ArtistId = 3, Duration = 150 },
+            };
+
+            // Setup mock repositories
+            mockArtistRepository.Setup(repo => repo.ReadAll()).Returns(artists.AsQueryable());
+            mockSongRepository.Setup(repo => repo.ReadAll()).Returns(songs.AsQueryable());
+
+            // Act
+            var result = service.GetSongsByDurationAndArtistGender(100, "Male");
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count());
+            Assert.IsTrue(result.All(song => song.Duration > 100 && artists.Any(a => a.ArtistId == song.ArtistId && a.Gender == "Male")));
+        }
+
         [Test]
         public void ReadAll_ShouldReturnAllItemsFromRepository()
         {
@@ -71,7 +111,7 @@ namespace GKKVWT_HFT_2023241.Test
             //ACT
             logic.Create(song);
             //ASSERT
-            mockMovieRepo.Verify(r => r.Create(song), Times.Once);
+            mockSongRepo.Verify(r => r.Create(song), Times.Once);
         }
 
         [Test]
@@ -99,7 +139,7 @@ namespace GKKVWT_HFT_2023241.Test
 
             }
             //ASSERT
-            mockMovieRepo.Verify(r => r.Create(song), Times.Never);
+            mockSongRepo.Verify(r => r.Create(song), Times.Never);
         }
     }
 }
