bd89659f530ec3370152c6a870f77f29a53cb717
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-06 18:01:32 +0100

    Base abstract Repository functions added.

--- GKKVWT_HFT_2023241.Repository/Repository/Repository.cs
+++ GKKVWT_HFT_2023241.Repository/Repository/Repository.cs
@@ -9,6 +9,21 @@ namespace GKKVWT_HFT_2023241.Repository.Repository
 {
     public abstract class Repository<T> : IRespository<T> where T : class
     {
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public IQueryable<T> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract T Read(int id);
+        public abstract void Update(T item);
     }
 }
