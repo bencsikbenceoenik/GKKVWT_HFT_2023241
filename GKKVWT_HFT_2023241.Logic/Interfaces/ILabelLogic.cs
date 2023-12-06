fc71951830e81864e1f5f57c736d7febf75d821f
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-06 18:38:51 +0100

    ILabelLogic interface CRUD functions added.

--- GKKVWT_HFT_2023241.Logic/Interfaces/ILabelLogic.cs
+++ GKKVWT_HFT_2023241.Logic/Interfaces/ILabelLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GKKVWT_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace GKKVWT_HFT_2023241.Logic.Interfaces
 {
     public interface ILabelLogic
     {
-
+        void Create(Label item);
+        void Delete(int id);
+        Label Read(int id);
+        IQueryable<Label> ReadAll();
+        void Update(Label item);
     }
 }
