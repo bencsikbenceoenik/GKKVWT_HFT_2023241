3dd721069f142d58d18c04a78b93de14823eb9af
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-06 19:00:09 +0100

    SongLogic CRUD functions and Constructor implemented. Dependency Injection added.

--- GKKVWT_HFT_2023241.Logic/Classes/SongLogic.cs
+++ GKKVWT_HFT_2023241.Logic/Classes/SongLogic.cs
@@ -1,4 +1,6 @@
 ﻿using GKKVWT_HFT_2023241.Logic.Interfaces;
+using GKKVWT_HFT_2023241.Models;
+using GKKVWT_HFT_2023241.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,36 @@ using System.Threading.Tasks;
 
 namespace GKKVWT_HFT_2023241.Logic.Classes
 {
-    public class SongLogic : ILabelLogic
+    public class SongLogic : ISongLogic
     {
+        IRepository<Song> repo;
+        public SongLogic(IRepository<Song> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Song item)
+        {
+            this.repo.Create(item);
+        }
 
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Song Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Song> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Song item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
