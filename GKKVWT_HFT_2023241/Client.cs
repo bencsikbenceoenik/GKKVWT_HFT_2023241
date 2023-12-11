1e6ce02870285aa21ffafcaa7047fcf27db4791f
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-11 19:53:14 +0100

    CRUD methods modified to use endpoint

--- GKKVWT_HFT_2023241/Client.cs
+++ GKKVWT_HFT_2023241/Client.cs
@@ -9,11 +9,13 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Numerics;
+using System.Xml.Linq;
 
 namespace GKKVWT_HFT_2023241.Client
 {
     public class Client
     {
+        static RestService rest;
         static SongLogic songLogic;
         static ArtistLogic artistLogic;
         static LabelLogic labelLogic;
@@ -25,17 +27,20 @@ namespace GKKVWT_HFT_2023241.Client
                 case "Song":
                     Console.WriteLine("Enter Song Name: ");
                     string songName = Console.ReadLine();
-                    songLogic.Create(new Song() { SongTitle = songName});
+                    //songLogic.Create(new Song() { SongTitle = songName});
+                    rest.Post(new Song() { SongTitle = songName }, "song");
                     break;
                 case "Artist":
                     Console.WriteLine("Enter Artist Name: ");
                     string artistName = Console.ReadLine();
-                    artistLogic.Create(new Artist() { ArtistName = artistName });
+                    //artistLogic.Create(new Artist() { ArtistName = artistName });
+                    rest.Post(new Artist() { ArtistName = artistName }, "artist");
                     break;
                 case "Label":
                     Console.WriteLine("Enter Label Name: ");
                     string labelName = Console.ReadLine();
-                    labelLogic.Create(new Label() { LabelName = labelName });
+                    //labelLogic.Create(new Label() { LabelName = labelName });
+                    rest.Post(new Label() { LabelName = labelName }, "label");
                     break;
             }
             Console.ReadLine();
@@ -43,20 +48,11 @@ namespace GKKVWT_HFT_2023241.Client
 
         static void List(string entity)
         {
-            //if (entity == "Actor")
-            //{
-            //    List<Actor> actors = rest.Get<Actor>("actor");
-            //    foreach (var item in actors)
-            //    {
-            //        Console.WriteLine(item.ActorId + ": " + item.ActorName);
-            //    }
-            //}
-
-
             switch (entity)
             {
                 case "Song":
-                    var songs = songLogic.ReadAll();
+                    //var songs = songLogic.ReadAll();
+                    var songs = rest.Get<Song>("song");
                     Console.WriteLine("Id" + "\t" + "Name");
                     foreach (var item in songs)
                     {
@@ -64,7 +60,8 @@ namespace GKKVWT_HFT_2023241.Client
                     }
                     break;
                 case "Artist":
-                    var artists = artistLogic.ReadAll();
+                    //var artists = artistLogic.ReadAll();
+                    var artists = rest.Get<Artist>("artist");
                     Console.WriteLine("Id" + "\t" + "Name");
                     foreach (var item in artists)
                     {
@@ -72,7 +69,8 @@ namespace GKKVWT_HFT_2023241.Client
                     }
                     break;
                 case "Label":
-                    var labels = labelLogic.ReadAll();
+                    //var labels = labelLogic.ReadAll();
+                    var labels = rest.Get<Label>("label");
                     Console.WriteLine("Id" + "\t" + "Name");
                     foreach (var item in labels)
                     {
@@ -80,22 +78,9 @@ namespace GKKVWT_HFT_2023241.Client
                     }
                     break;
             }
-            Console.ReadLine();
         }
         static void Update(string entity)
         {
-
-            //if (entity == "Actor")
-            //{
-            //    Console.Write("Enter Actor's id to update: ");
-            //    int id = int.Parse(Console.ReadLine());
-            //    Actor one = rest.Get<Actor>(id, "actor");
-            //    Console.Write($"New name [old: {one.ActorName}]: ");
-            //    string name = Console.ReadLine();
-            //    one.ActorName = name;
-            //    rest.Put(one, "actor");
-            //}
-
             switch (entity)
             {
                 case "Artist":
@@ -105,7 +90,8 @@ namespace GKKVWT_HFT_2023241.Client
                     Console.Write($"New name [old: {artistOne.ArtistName}]: ");
                     string artistName = Console.ReadLine();
                     artistOne.ArtistName = artistName;
-                    artistLogic.Update(artistOne);
+                    //artistLogic.Update(artistOne);
+                    rest.Put(artistOne, "artist");
                     break;
                 case "Song":
                     Console.Write("Enter Song's id to update: ");
@@ -114,7 +100,8 @@ namespace GKKVWT_HFT_2023241.Client
                     Console.Write($"New name [old: {songOne.SongId}]: ");
                     string songName = Console.ReadLine();
                     songOne.SongTitle = songName;
-                    songLogic.Update(songOne);
+                    //songLogic.Update(songOne);
+                    rest.Put(songOne, "song");
                     break;
                 case "Label":
                     Console.Write("Enter Label's id to update: ");
@@ -123,43 +110,39 @@ namespace GKKVWT_HFT_2023241.Client
                     Console.Write($"New name [old: {labelOne.LabelName}]: ");
                     string labelName = Console.ReadLine();
                     labelOne.LabelName = labelName;
-                    labelLogic.Update(labelOne);
+                    //labelLogic.Update(labelOne);
+                    rest.Put(labelOne, "label");
                     break;
             }
             Console.ReadLine();
         }
         static void Delete(string entity)
         {
-
-            //if (entity == "Actor")
-            //{
-            //    Console.Write("Enter Actor's id to delete: ");
-            //    int id = int.Parse(Console.ReadLine());
-            //    rest.Delete(id, "actor");
-            //}
-
             switch (entity)
             {
                 case "Artist":
                     Console.WriteLine("Enter Artist's id to delete: ");
                     int artistId = int.Parse(Console.ReadLine());
-                    artistLogic.Delete(artistId);
+                    //artistLogic.Delete(artistId);
+                    rest.Delete(artistId, "artist");
                     break;
                 case "Song":
                     Console.WriteLine("Enter Song's id to delete: ");
                     int songId = int.Parse(Console.ReadLine());
-                    songLogic.Delete(songId);
+                    //songLogic.Delete(songId);
+                    rest.Delete(songId, "song");
                     break;
                 case "Label":
                     Console.WriteLine("Enter Label's id to delete: ");
                     int labelId = int.Parse(Console.ReadLine());
-                    labelLogic.Delete(labelId);
+                    //labelLogic.Delete(labelId);
+                    rest.Delete(labelId, "label");
                     break;
             }
-            Console.ReadLine();
         }
         static void Main(string[] args)
         {
+            rest = new RestService("http://localhost:53910/", "song");
             var ctx = new SongDbContext();
 
             var songRepo = new SongRepository(ctx);
