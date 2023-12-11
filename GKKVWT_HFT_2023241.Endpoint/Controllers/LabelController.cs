36bfe139d163b511c86ba2c1b73b0f3e97836e4d
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-11 18:08:35 +0100

    LabelController added and coded.

---
+++ GKKVWT_HFT_2023241.Endpoint/Controllers/LabelController.cs
@@ -0,0 +1,48 @@
+﻿using GKKVWT_HFT_2023241.Logic.Interfaces;
+using GKKVWT_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GKKVWT_HFT_2023241.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LabelController : ControllerBase
+    {
+        ILabelLogic logic;
+        public LabelController(ILabelLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Label> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Label Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Label value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Label value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
