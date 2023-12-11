44f187ecfc3f51c4484cb958b0810185024ad962
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-11 19:24:38 +0100

    Microsoft.AspNet.WebApi.Client nuget package added restservice.cs content comments removed.

--- GKKVWT_HFT_2023241.Endpoint/Startup.cs
+++ GKKVWT_HFT_2023241.Endpoint/Startup.cs
@@ -5,7 +5,9 @@ using GKKVWT_HFT_2023241.Repository.Database;
 using GKKVWT_HFT_2023241.Repository.Interface;
 using GKKVWT_HFT_2023241.Repository.ModelRepositories;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -58,6 +60,15 @@ namespace GKKVWT_HFT_2023241.Endpoint
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GKKVWT_HFT_2023241.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
