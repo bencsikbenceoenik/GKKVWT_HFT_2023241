3e4a4262d18c91b6877fc98a3ebf9da93b716acf
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-11 17:57:00 +0100

    SongController added and coded.

---
+++ GKKVWT_HFT_2023241.Endpoint/Controllers/SongController.cs
@@ -0,0 +1,48 @@
+﻿using GKKVWT_HFT_2023241.Logic.Interfaces;
+using GKKVWT_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GKKVWT_HFT_2023241.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        ISongLogic logic;
+        public SongController(ISongLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Song> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Song Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Song value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Song value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
