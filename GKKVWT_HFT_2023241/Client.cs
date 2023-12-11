b69fb3884875bf8259361d93d3377e1283777f3c
Bencsik Bence <bence.bencsik@stud.uni-obuda.hu>
2023-12-11 20:02:15 +0100

    CRUD endpoint version completed.

--- GKKVWT_HFT_2023241/Client.cs
+++ GKKVWT_HFT_2023241/Client.cs
@@ -1,9 +1,5 @@
 ﻿using ConsoleTools;
-using GKKVWT_HFT_2023241.Logic.Classes;
-using GKKVWT_HFT_2023241.Logic.Interfaces;
 using GKKVWT_HFT_2023241.Models;
-using GKKVWT_HFT_2023241.Repository.Database;
-using GKKVWT_HFT_2023241.Repository.ModelRepositories;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -16,9 +12,6 @@ namespace GKKVWT_HFT_2023241.Client
     public class Client
     {
         static RestService rest;
-        static SongLogic songLogic;
-        static ArtistLogic artistLogic;
-        static LabelLogic labelLogic;
 
         static void Create(string entity)
         {
@@ -27,19 +20,16 @@ namespace GKKVWT_HFT_2023241.Client
                 case "Song":
                     Console.WriteLine("Enter Song Name: ");
                     string songName = Console.ReadLine();
-                    //songLogic.Create(new Song() { SongTitle = songName});
                     rest.Post(new Song() { SongTitle = songName }, "song");
                     break;
                 case "Artist":
                     Console.WriteLine("Enter Artist Name: ");
                     string artistName = Console.ReadLine();
-                    //artistLogic.Create(new Artist() { ArtistName = artistName });
                     rest.Post(new Artist() { ArtistName = artistName }, "artist");
                     break;
                 case "Label":
                     Console.WriteLine("Enter Label Name: ");
                     string labelName = Console.ReadLine();
-                    //labelLogic.Create(new Label() { LabelName = labelName });
                     rest.Post(new Label() { LabelName = labelName }, "label");
                     break;
             }
@@ -51,7 +41,6 @@ namespace GKKVWT_HFT_2023241.Client
             switch (entity)
             {
                 case "Song":
-                    //var songs = songLogic.ReadAll();
                     var songs = rest.Get<Song>("song");
                     Console.WriteLine("Id" + "\t" + "Name");
                     foreach (var item in songs)
@@ -60,7 +49,6 @@ namespace GKKVWT_HFT_2023241.Client
                     }
                     break;
                 case "Artist":
-                    //var artists = artistLogic.ReadAll();
                     var artists = rest.Get<Artist>("artist");
                     Console.WriteLine("Id" + "\t" + "Name");
                     foreach (var item in artists)
@@ -69,7 +57,6 @@ namespace GKKVWT_HFT_2023241.Client
                     }
                     break;
                 case "Label":
-                    //var labels = labelLogic.ReadAll();
                     var labels = rest.Get<Label>("label");
                     Console.WriteLine("Id" + "\t" + "Name");
                     foreach (var item in labels)
@@ -86,31 +73,28 @@ namespace GKKVWT_HFT_2023241.Client
                 case "Artist":
                     Console.Write("Enter Actor's id to update: ");
                     int artistId = int.Parse(Console.ReadLine());
-                    Artist artistOne = artistLogic.Read(artistId);
+                    Artist artistOne = rest.Get<Artist>(artistId, "artist");
                     Console.Write($"New name [old: {artistOne.ArtistName}]: ");
                     string artistName = Console.ReadLine();
                     artistOne.ArtistName = artistName;
-                    //artistLogic.Update(artistOne);
                     rest.Put(artistOne, "artist");
                     break;
                 case "Song":
                     Console.Write("Enter Song's id to update: ");
                     int songId = int.Parse(Console.ReadLine());
-                    Song songOne = songLogic.Read(songId);
+                    Song songOne = rest.Get<Song>(songId, "song");
                     Console.Write($"New name [old: {songOne.SongId}]: ");
                     string songName = Console.ReadLine();
                     songOne.SongTitle = songName;
-                    //songLogic.Update(songOne);
                     rest.Put(songOne, "song");
                     break;
                 case "Label":
                     Console.Write("Enter Label's id to update: ");
                     int labelId = int.Parse(Console.ReadLine());
-                    Label labelOne = labelLogic.Read(labelId);
+                    Label labelOne = rest.Get<Label>(labelId, "label");
                     Console.Write($"New name [old: {labelOne.LabelName}]: ");
                     string labelName = Console.ReadLine();
                     labelOne.LabelName = labelName;
-                    //labelLogic.Update(labelOne);
                     rest.Put(labelOne, "label");
                     break;
             }
@@ -123,19 +107,16 @@ namespace GKKVWT_HFT_2023241.Client
                 case "Artist":
                     Console.WriteLine("Enter Artist's id to delete: ");
                     int artistId = int.Parse(Console.ReadLine());
-                    //artistLogic.Delete(artistId);
                     rest.Delete(artistId, "artist");
                     break;
                 case "Song":
                     Console.WriteLine("Enter Song's id to delete: ");
                     int songId = int.Parse(Console.ReadLine());
-                    //songLogic.Delete(songId);
                     rest.Delete(songId, "song");
                     break;
                 case "Label":
                     Console.WriteLine("Enter Label's id to delete: ");
                     int labelId = int.Parse(Console.ReadLine());
-                    //labelLogic.Delete(labelId);
                     rest.Delete(labelId, "label");
                     break;
             }
@@ -143,15 +124,6 @@ namespace GKKVWT_HFT_2023241.Client
         static void Main(string[] args)
         {
             rest = new RestService("http://localhost:53910/", "song");
-            var ctx = new SongDbContext();
-
-            var songRepo = new SongRepository(ctx);
-            var artistRepo = new ArtistRepository(ctx);
-            var labelRepo = new LabelRepository(ctx);
-
-            songLogic = new SongLogic(songRepo);
-            artistLogic = new ArtistLogic(artistRepo);
-            labelLogic = new LabelLogic(labelRepo);
 
             var songSubMenu = new ConsoleMenu(args, level: 1)
                 .Add("List", () => List("Song"))
