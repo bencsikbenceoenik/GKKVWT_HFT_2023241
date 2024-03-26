36d53865470b476d13dd9850d9854f967f23be1e
bencsikbenceoenik <bencsikbence01@gmail.com>
2024-03-26 09:42:27 +0100

    RestCollection and ViewModel for Artists are done

---
+++ WPF_Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,99 @@
+﻿using GKKVWT_HFT_2023241.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF_Client.ViewModels
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        private string errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { SetProperty(ref errorMessage, value); }
+        }
+
+        public RestCollection<Artist> Artists { get; set; }
+
+        private Artist selectedArtist;
+
+        public Artist SelectedArtist
+        {
+            get { return selectedArtist; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedArtist = new Artist()
+                    {
+                        ArtistName = value.ArtistName,
+                        ArtistId = value.ArtistId
+                    };
+                    OnPropertyChanged();
+                    (DeleteArtistCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+        public ICommand CreateArtistCommand { get; set; }
+        public ICommand DeleteArtistCommand { get; set; }
+        public ICommand UpdateArtistCommand { get; set; }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Artists = new RestCollection<Artist>("http://localhost:40338/","artist","hub");
+                CreateArtistCommand = new RelayCommand(() =>
+                {
+                    Artists.Add(new Artist()
+                    {
+                        ArtistName = SelectedArtist.ArtistName
+                    });
+                });
+
+                UpdateArtistCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Artists.Update(SelectedArtist);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ErrorMessage = ex.Message;
+                    }
+                });
+
+                DeleteArtistCommand = new RelayCommand(() =>
+                {
+                    Artists.Delete(SelectedArtist.ArtistId);
+                },
+                () =>
+                {
+                    return SelectedArtist != null;
+                });
+                SelectedArtist = new Artist();
+                ;
+            }
+        }
+
+    }
+}
