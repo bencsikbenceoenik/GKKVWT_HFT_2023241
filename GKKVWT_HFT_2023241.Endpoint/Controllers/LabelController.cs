ffa7959340195b7d9063fec86bfc599b0380b293
bencsikbenceoenik <bencsikbence01@gmail.com>
2024-04-09 09:25:59 +0200

    Song and label controller done for signal hub

--- GKKVWT_HFT_2023241.Endpoint/Controllers/LabelController.cs
+++ GKKVWT_HFT_2023241.Endpoint/Controllers/LabelController.cs
@@ -1,6 +1,8 @@
-﻿using GKKVWT_HFT_2023241.Logic.Interfaces;
+﻿using GKKVWT_HFT_2023241.Endpoint.Services;
+using GKKVWT_HFT_2023241.Logic.Interfaces;
 using GKKVWT_HFT_2023241.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 
 namespace GKKVWT_HFT_2023241.Endpoint.Controllers
@@ -10,9 +12,11 @@ namespace GKKVWT_HFT_2023241.Endpoint.Controllers
     public class LabelController : ControllerBase
     {
         ILabelLogic logic;
-        public LabelController(ILabelLogic logic)
+        IHubContext<SignalRHub> hub;
+        public LabelController(ILabelLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -31,18 +35,22 @@ namespace GKKVWT_HFT_2023241.Endpoint.Controllers
         public void Create([FromBody] Label value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("LabelCreated", value);
         }
 
         [HttpPut]
         public void Put([FromBody] Label value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("LabelUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var labelToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("LabelDeleted",labelToDelete);
         }
     }
 }
