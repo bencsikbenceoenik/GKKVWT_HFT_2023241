d510c8ea70533add58da30f1d76e9591801df863
bencsikbenceoenik <bencsikbence01@gmail.com>
2024-04-09 09:19:15 +0200

    artist controller done with signal hub

--- GKKVWT_HFT_2023241.Endpoint/Controllers/ArtistController.cs
+++ GKKVWT_HFT_2023241.Endpoint/Controllers/ArtistController.cs
@@ -1,6 +1,9 @@
-﻿using GKKVWT_HFT_2023241.Logic.Interfaces;
+﻿using GKKVWT_HFT_2023241.Endpoint.Services;
+using GKKVWT_HFT_2023241.Logic.Interfaces;
 using GKKVWT_HFT_2023241.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 namespace GKKVWT_HFT_2023241.Endpoint.Controllers
@@ -10,9 +13,11 @@ namespace GKKVWT_HFT_2023241.Endpoint.Controllers
     public class ArtistController : ControllerBase
     {
         IArtistLogic logic;
-        public ArtistController(IArtistLogic logic)
+        IHubContext<SignalRHub> hub;
+        public ArtistController(IArtistLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -31,18 +36,22 @@ namespace GKKVWT_HFT_2023241.Endpoint.Controllers
         public void Create([FromBody] Artist value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("ArtistCreated", value);
         }
 
         [HttpPut]
         public void Put([FromBody] Artist value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("ArtistUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var artistToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("ArtistDeleted", artistToDelete);
         }
     }
 }
