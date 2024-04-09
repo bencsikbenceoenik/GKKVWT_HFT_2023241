b991aa938c8c3c99ea0dd691478494e2a7bdb77d
bencsikbenceoenik <bencsikbence01@gmail.com>
2024-04-09 08:47:59 +0200

    Viewmodel Song and Label part added

--- WPF_Client/MainWindow.xaml.cs
+++ WPF_Client/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GKKVWT_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,9 +22,26 @@ namespace WPF_Client
     public partial class MainWindow : Window
     {
         RestService rest;
+        List<Artist> artists;
         public MainWindow()
         {
             InitializeComponent();
+            artists = new List<Artist>()
+            {
+                new Artist("1#BTS#1993-12-04#2013#Group#Male#South Korean#"),
+                new Artist("2#BLACKPINK#1996-01-03#2016#Group#Female#South Korean#"),
+                new Artist("3#EXO#1992-09-21#2012#Group#Male#South Korean#"),
+                new Artist("4#TWICE#1999-02-01#2015#Group#Female#South Korean#"),
+                new Artist("5#RED VELVET#1991-03-29#2014#Group#Female#South Korean#"),
+                new Artist("6#ITZY#2000-08-20#2019#Group#Female#South Korean#"),
+                new Artist("7#NCT 127#1995-07-01#2016#Group#Male#South Korean#"),
+                new Artist("8#STRAY KIDS#2000-09-14#2018#Group#Male#South Korean#"),
+                new Artist("9#EXID#1991-08-12#2012#Group#Female#South Korean#"),
+                new Artist("10#MONSTA X#1993-01-26#2015#Group#Male#South Korean#"),
+                new Artist("11#MAMAMOO#1991-06-19#2014#Group#Female#South Korean#"),
+                new Artist("12#(G)I-DLE#1998-08-14#2018#Group#Female#South Korean#")
+            };
+            ltb.ItemsSource = artists;
         }
     }
 }
--- WPF_Client/ViewModels/MainWindowViewModel.cs
+++ WPF_Client/ViewModels/MainWindowViewModel.cs
@@ -23,6 +23,7 @@ namespace WPF_Client.ViewModels
             set { SetProperty(ref errorMessage, value); }
         }
 
+        #region Artist
         public RestCollection<Artist> Artists { get; set; }
 
         private Artist selectedArtist;
@@ -48,6 +49,64 @@ namespace WPF_Client.ViewModels
         public ICommand DeleteArtistCommand { get; set; }
         public ICommand UpdateArtistCommand { get; set; }
 
+        #endregion
+
+        #region Song
+        public RestCollection<Song> Songs { get; set; }
+
+        private Song selectedSong;
+
+        public Song SelectedSong
+        {
+            get { return selectedSong; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedSong = new Song()
+                    {
+                        SongTitle = value.SongTitle,
+                        SongId = value.SongId
+                    };
+                    OnPropertyChanged();
+                    (DeleteSongCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+        public ICommand CreateSongCommand { get; set; }
+        public ICommand DeleteSongCommand { get; set; }
+        public ICommand UpdateSongCommand { get; set; }
+
+        #endregion
+
+        #region Label
+        public RestCollection<Label> Labels { get; set; }
+
+        private Label selectedLabel;
+
+        public Label SelectedLabel
+        {
+            get { return selectedLabel; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedLabel = new Label()
+                    {
+                        LabelName = value.LabelName,
+                        LabelId = value.LabelId
+                    };
+                    OnPropertyChanged();
+                    (DeleteLabelCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+        public ICommand CreateLabelCommand { get; set; }
+        public ICommand DeleteLabelCommand { get; set; }
+        public ICommand UpdateLabelCommand { get; set; }
+
+        #endregion
+
         public static bool IsInDesignMode
         {
             get
@@ -61,7 +120,9 @@ namespace WPF_Client.ViewModels
         {
             if (!IsInDesignMode)
             {
-                Artists = new RestCollection<Artist>("http://localhost:40338/","artist","hub");
+                #region Artist
+
+                Artists = new RestCollection<Artist>("http://localhost:40338/", "artist", "hub");
                 CreateArtistCommand = new RelayCommand(() =>
                 {
                     Artists.Add(new Artist()
@@ -91,7 +152,79 @@ namespace WPF_Client.ViewModels
                     return SelectedArtist != null;
                 });
                 SelectedArtist = new Artist();
-                ;
+
+                #endregion
+
+                #region Song
+
+                Songs = new RestCollection<Song>("http://localhost:40338/", "song", "hub");
+                CreateSongCommand = new RelayCommand(() =>
+                {
+                    Songs.Add(new Song()
+                    {
+                        SongTitle = SelectedSong.SongTitle
+                    });
+                });
+
+                UpdateSongCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Songs.Update(SelectedSong);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ErrorMessage = ex.Message;
+                    }
+                });
+
+                DeleteSongCommand = new RelayCommand(() =>
+                {
+                    Songs.Delete(SelectedSong.SongId);
+                },
+                () =>
+                {
+                    return SelectedSong != null;
+                });
+                SelectedSong = new Song();
+
+                #endregion
+
+                #region Label
+
+                Artists = new RestCollection<Artist>("http://localhost:40338/", "label", "hub");
+                CreateLabelCommand = new RelayCommand(() =>
+                {
+                    Labels.Add(new Label()
+                    {
+                        LabelName = SelectedLabel.LabelName
+                    });
+                });
+
+                UpdateLabelCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Labels.Update(SelectedLabel);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ErrorMessage = ex.Message;
+                    }
+                });
+
+                DeleteLabelCommand = new RelayCommand(() =>
+                {
+                    Labels.Delete(SelectedLabel.LabelId);
+                },
+                () =>
+                {
+                    return SelectedLabel != null;
+                });
+                SelectedLabel = new Label();
+
+                #endregion
+
             }
         }
 
